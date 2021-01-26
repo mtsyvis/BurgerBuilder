da5b907d19fe91258578d10fbde3b1c39db30ca1
Mikhail Tsyvis <mikhail_tsyvis@epam.com>
2021-01-26 17:26:21 +0300

    Reusing the Backdrop and my own realizetion of sideDrawer toggle

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,11 +5,11 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>Menu</div>
+        <div onClick={props.menuClicked}>Menu</div>
         <div className={classes.Logo}>
             <Logo />
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
