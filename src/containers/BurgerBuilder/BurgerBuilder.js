240dad67bf3172cf701469365092dbb997f0206a
Mikhail Tsyvis <mikhail_tsyvis@epam.com>
2021-02-04 23:16:00 +0300

    Sending a POST Request finish

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -80,7 +81,27 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+
+        const order = {
+            ingridients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Mikhail Tsyvis',
+                address: {
+                    city: 'Minsk',
+                    zipCode: '1124',
+                    country: 'Belarus'
+                },
+                email: 'test@mail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => console.log(error));
     }
 
 
